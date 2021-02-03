4f38eaac5be8b37d7df8d5be825b8265b9d6cf61
Aleksei Iudin <aleksei.iudin@stm-labs.ru>
2021-02-03 10:20:32 +0300

    feat: add components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FilterComponent } from './components/filter/filter.component';
+import { ResultListComponent } from './components/result-list/result-list.component';
+import { ResultItemComponent } from './components/result-item/result-item.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FilterComponent,
+    ResultListComponent,
+    ResultItemComponent
   ],
   imports: [
     BrowserModule,
